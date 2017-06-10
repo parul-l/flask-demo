7dc3bc4b5eeb3842b095c5b141d52f3ecb8faf35
Parul <parul.laul@gmail.com>
2017-06-10 15:57:43 -0400

    changed port

--- app.py
+++ app.py
@@ -36,5 +36,8 @@ def error_page():
     return render_template('error_page.html')
 
 
-if __name__ == '__main__':
-    app.run(port=33507)
+# if __name__ == '__main__':
+#     app.run(port=33507)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
