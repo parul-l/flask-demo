f6a55d4e736ee9648ffc00f5d71df8d3f742f115
Parul <parul.laul@gmail.com>
2017-06-10 17:32:19 -0400

    Lets try this

--- plot_stock_prices.py
+++ plot_stock_prices.py
@@ -4,8 +4,8 @@ from bokeh.embed import components
 
 def get_plot_ticker_components(ticker, df_data):
     plot_title = 'Last Month ' + ticker.upper() + ' Closing Price'
-
-    p = TimeSeries(df_data, legend=True, title=plot_title, xlabel='Date', ylabel='Stock Prices')
+    print(df_data)
+    p = TimeSeries(df_data.closing_price, legend=True, title=plot_title, xlabel='Date', ylabel='Stock Prices')
     p.title.text_font_size = '14pt'
 
     script, div = components(p)
